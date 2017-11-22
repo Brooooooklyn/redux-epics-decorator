6afb188d3d7e37a6b15e570875baa80e36843ee0
LongYinan <lynweklm@gmail.com>
2017-11-22 22:55:50 +0800

    fix(Type): still stop call props.defaultState as a DispatchProps

--- src/interface.ts
+++ src/interface.ts
@@ -4,8 +4,13 @@ import { MiddlewareAPI } from 'redux'
 
 import { EffectModule } from './EffectModule'
 
+// https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-311923766 would break defination lookup
+// just a workaround to avoid `props.reducer()`
+// need refactor
 export type ModuleActionProps <S, T extends EffectModule<S>> = {
   [key in keyof T]: (...args: any[]) => Action<any>
+} & {
+  defaultState: never
 }
 
 export interface EpicLike<Input, Output, S, ActionType extends string> {
